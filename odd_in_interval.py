4ace7aa9808e2ee8af4db7981958ef31f2ed891f
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-08 12:33:27 +0530

    Update odd_in_interval.py

--- odd_in_interval.py
+++ odd_in_interval.py
@@ -1,8 +1,12 @@
 start,end=map(int,input().split())
+l=[]
 if start%2==0:
     start+=1
 else:
     start+=2
 while start<end:
-    print(start,end=' ')
+    l.append(start)
     start+=2
+for i in range(len(l)-1):
+    print(l[i],end='')
+print(l[len(l)-1]
