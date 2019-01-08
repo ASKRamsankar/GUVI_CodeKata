ad24b8bb5f7b52b4dae09cf035ae8c7a0121220a
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-08 12:38:19 +0530

    Update even_in_interval.py

--- even_in_interval.py
+++ even_in_interval.py
@@ -1,8 +1,12 @@
 start,end=map(int,input().split())
+l=[]
 if start%2==0:
     start+=2
 else:
     start+=1
 while start<end:
-    print(start)
+    l.append(start)
     start+=2
+for i in range(len(l)-1):
+    print(l[i],end=' ')
+print(l[len(l)-1])
