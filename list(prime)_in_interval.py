124e6cbc2ebe3a01bb1e9e6174c68d93b92d0e3d
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-08 11:57:52 +0530

    Create list(prime)_in_interval.py

---
+++ list(prime)_in_interval.py
@@ -0,0 +1,15 @@
+start,end=map(int,input().split())
+l=[]
+def isprime(no):
+  for j in range(2,no):
+    if no%j==0:
+      return False
+  else:
+    if j+1==no:
+      return True
+for i in range(start+1,end):
+  if isprime(i):
+    l.append(i)
+for i in range(len(l)-1):
+  print(l[i],end=' ')
+print(l[i+1])
