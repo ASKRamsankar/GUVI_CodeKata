d58b3bdf01ff2e97ab2de66157249e6a76797bb6
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-09 23:42:06 +0530

    Update equalize_cost2.py

--- equalize_cost2.py
+++ equalize_cost2.py
@@ -4,7 +4,7 @@ if len(s1)>len(s2):
   s1,s2=s2,s1
 i=0
 while i<len(s1):
-  s+=abs(ord(s1[i])-ord(s2[i]))
+  s+=(ord(s2[i])-ord(s1[i]))
   i+=1
 for i in range(i,len(s2)):
   s+=ord(s2[i])-ord('a')+1
