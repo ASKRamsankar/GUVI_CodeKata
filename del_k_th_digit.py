77717bab4193675a9d0e3edbb076dbfc2388251f
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-09 18:18:12 +0530

    Update del_k_th_digit.py

--- del_k_th_digit.py
+++ del_k_th_digit.py
@@ -1,17 +1,7 @@
 n,k=input().split()
 k=int(k)
-min1=10000
-def remove(inx):
-    s=''
-    for i in range(len(n)):
-        if i<inx+k and i>=inx:
-            continue
-        else:
-            s+=n[i]
-    return s
-for i in range(len(n)):
-    value=remove(i)
-    if min1>int(value):
-        min1=int(value)
-print(min1)
-    
+m=1000000
+for i in range(len(n)-k):
+    if int(n[i:i+k])<m:
+        m=int(n[i:i+k])
+print(m)    
