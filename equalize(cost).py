7ce17d09ba2afc983b05ffa009b9e8ab17641f0b
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-09 23:02:14 +0530

    Update equalize(cost).py

--- equalize(cost).py
+++ equalize(cost).py
@@ -1,11 +1,18 @@
 s1,s2=input().split()
-if len(s1)>len(s2):
-  m=len(s1)
-else:
-  m=len(s2)
+s1=list(s1)
+s2=list(s2)
+m=abs(len(s1)-len(s2))
 i=0
-while i<len(s1) and i<len(s2):
-  if s1[i]==s2[i]:
-    m-=1
-  i+=1
+j=0
+while i<len(s1) and j<len(s2):
+  if s1[i]==s2[j]:
+    i+=1
+    j+=1
+  else:
+    if len(s1)>len(s2):
+      s1.pop(i)
+    else:
+      i+=1
+      j+=1
+      m+=1
 print(m)
