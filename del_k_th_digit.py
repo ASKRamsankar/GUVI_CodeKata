0b3f77ff729f6c8055b0e535211873041d35eb1c
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-09 18:22:43 +0530

    Update del_k_th_digit.py

--- del_k_th_digit.py
+++ del_k_th_digit.py
@@ -1,7 +1,10 @@
 n,k=input().split()
 k=int(k)
-m=1000000
-for i in range(len(n)-k):
-    if int(n[i:i+k])<m:
-        m=int(n[i:i+k])
-print(m)    
+m=100000000000000000000000000000000
+if k==0:
+    print(n)
+else:
+    for i in range(len(n)-k):
+        if int(n[i:i+k])<m:
+            m=int(n[i:i+k])
+    print(m)    
