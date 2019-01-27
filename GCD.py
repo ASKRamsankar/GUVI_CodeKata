3779c43d84d795f3c39adc464e1acc1b8af0181a
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-27 22:01:50 +0530

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,21 @@
+n1,n2=map(int,input().split())
+flag=0
+gcd=1
+def find(n,m):
+  global n1,n2,flag
+  i=0
+  for i in range(2,min(n,m)+1):
+    if n%i==0 and m%i==0:
+      n/=i
+      n1=int(n)
+      m/=i
+      n2=int(m)
+      return i
+  else:
+      flag=1
+      #return (m*n)
+      return 1
+
+while flag!=1:
+  gcd*=(find(n1,n2))
+print(gcd)
