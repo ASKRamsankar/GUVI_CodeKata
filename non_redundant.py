ecc1844e6578791a394a58a9f96f406d19ba9ac3
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-29 19:20:17 +0530

    Create non_redundant.py

---
+++ non_redundant.py
@@ -0,0 +1,12 @@
+n=int(input())
+string=list(map(int,input().split()))
+num=[]
+count=[]
+for i in string:
+    if i not in num:
+        num.append(i)
+        count.append(1)
+    else:
+        count[num.index(i)]+=1
+
+print(num[count.index(1)]) 
