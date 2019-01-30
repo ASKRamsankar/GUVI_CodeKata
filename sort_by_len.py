f874475187912bc97f20b522d4f53ef30e47ba98
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-30 20:21:44 +0530

    Update sort_by_len.py

--- sort_by_len.py
+++ sort_by_len.py
@@ -1,14 +1,18 @@
 n=int(input())
 strings=input().split()
-for i in range(len(strings)-1):
-  if len(strings[i])>len(strings[i+1]):
-    strings[i],strings[i+1]=strings[i+1],strings[i]
+for i in range(len(strings)):
+  for j in range(0,n-i-1):
+    if len(strings[j])>len(strings[j+1]):
+      strings[j],strings[j+1]=strings[j+1],strings[j]
+
 for i in range(len(strings)-1):
   if len(strings[i])==len(strings[i+1]):
     a=strings[i]
     b=strings[i+1]
     for j in range(len(strings[i])):
-      if a[j]>b[j]:
+      if a[j]<b[j]:
+        break
+      elif a[j]>b[j]:
         strings[i],strings[i+1]=strings[i+1],strings[i]
         break
 print(' '.join(strings))
