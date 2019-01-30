6a375acdae2e5268a3c859b7d56bee6cf48e53c7
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-30 23:41:40 +0530

    Update odd in even.py

--- odd in even.py
+++ odd in even.py
@@ -1,14 +1,14 @@
 n=int(input())
 nos=input().split()
-no=[]
+no=''
 c=0
 for i in nos:
   if c==0:
     if int(i)%2==1:
-      no.append(i)
+      no+=i+' '
     c=1
   elif c==1:
     if int(i)%2==0:
-      no.append(i)
+      no+=i+' '
     c=0
-print(' '.join(no))
+print(no[:-1].strip())
