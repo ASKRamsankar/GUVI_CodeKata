001e255118c5f825124338ccb824dd6c66b4b837
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-31 19:30:50 +0530

    Create find_min_letter.py

---
+++ find_min_letter.py
@@ -0,0 +1,13 @@
+string=input()
+s=[]
+d={}
+for i in string:
+  if i not in d:
+    d[i]=1
+  else:
+    d[i]+=1
+m=min(d.values())
+for i in d.keys():
+  if d[i]==m and i!=' ':
+    s.append(i)
+print(*s)
