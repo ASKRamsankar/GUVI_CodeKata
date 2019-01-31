1f5b0cc4e0dcec1c854863e0ce7d04b52a4d5b49
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-01-31 19:15:59 +0530

    Update island.py

--- island.py
+++ island.py
@@ -1,31 +1,30 @@
 n=int(input())
 M=[]
+c=0
 for i in range(n):
   M.append(list(map(int,input().split())))
-print(M)
+f=0
 for i in range(len(M)):
   for j in range(len(M[0])):
+    f=0
     if M[i][j]==1:
-      try:
-        if M[i-1][j]==0:
-          f+=1
-      except:
-        pass
-      try:
-        if M[i+1][j]==0:
-          f+=1
-      except:
-        pass
-      try:
-        if M[i][j-1]==0:
-          f+=1
-      except:
-        pass
-      try:
-        if M[i][j+1]==0:
-          f+=1
-      except:
-        pass
+      if i-1<0:
+        f+=1
+      elif M[i-1][j]==0:
+        f+=1
+      if i+1>=len(M):
+        f+=1
+      elif M[i+1][j]==0:
+        f+=1
+      if j-1<0:
+        f+=1
+      elif M[i][j-1]==0:
+        f+=1
+      if j+1>=len(M):
+        f+=1
+      elif M[i][j+1]==0:
+        f+=1
     if f==4:
-      print(1)
-    
+      c+=1
+
+print(c)
