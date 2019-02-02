37e0951d702b26ebf62bf2b99325cc72b6cf0180
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-02 22:24:28 +0530

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,12 @@
+from itertools import permutations 
+l=list(input()) 
+print(l)
+perm = permutations(l) 
+array=[]  
+for i in list(perm):
+  s='' 
+  for j in i:
+    s+=j
+  if s not in array:
+    array.append(s)
+    print(s)
