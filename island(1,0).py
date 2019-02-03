6b598ec60de0a3da7924bf7d0886180bc8426b47
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-03 15:12:27 +0530

    Update island(1,0).py

--- island(1,0).py
+++ island(1,0).py
@@ -9,23 +9,47 @@ for i in range(len(M)):
   for j in range(len(M[0])):
     f=0
     if M[i][j]==1:
-      if i-1<0:
+      try:
+        if M[i-1][j]==0:
+          f+=1
+      except IndexError:
         f+=1
-      elif M[i-1][j]==0:
+      try:
+        if M[i+1][j]==0:
+          f+=1
+      except IndexError:
         f+=1
-      if i+1>=len(M):
+      try:
+        if M[i][j-1]==0:
+          f+=1
+      except IndexError:
         f+=1
-      elif M[i+1][j]==0:
+      try:
+        if M[i][j+1]==0:
+          f+=1
+      except IndexError:
         f+=1
-      if j-1<0:
+      try:
+        if M[i-1][j-1]==0:
+          f+=1
+      except IndexError:
         f+=1
-      elif M[i][j-1]==0:
+      try:
+        if M[i-1][j+1]==0:
+          f+=1
+      except IndexError:
         f+=1
-      if j+1>=len(M):
+      try:
+        if M[i+1][j-1]==0:
+          f+=1
+      except IndexError:
         f+=1
-      elif M[i][j+1]==0:
+      try:
+        if M[i+1][j+1]==0:
+          f+=1
+      except IndexError:
         f+=1
-    if f==4:
+    if f==8:
       c+=1
 
 print(c)
