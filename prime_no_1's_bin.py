35e3c07173c55dbdd3d6df585c1fb4f52be5496b
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-04 07:02:06 +0530

    Create prime_no_1's_bin.py

---
+++ prime_no_1's_bin.py
@@ -0,0 +1,25 @@
+def is_prime(no):
+  if no==1:
+    return True
+  else:
+    for i in range(2,no):
+      if no%i==0:
+        return False
+    else:
+      return True
+
+l,r=map(int,input().split())
+count=0
+
+for i in range(l,r+1):
+  string='{:2b}'.format(i)
+  print(string)
+  c=0
+  for j in string:
+    if j=='1':
+      c+=1
+  print(c)
+  if is_prime(c):
+    print('prime')
+    count+=1
+print(count)
