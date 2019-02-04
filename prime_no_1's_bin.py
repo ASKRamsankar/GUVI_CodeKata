8538352a3cc21e2f90ac844d23cc784f535efd1f
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-04 07:03:44 +0530

    Update prime_no_1's_bin.py

--- prime_no_1's_bin.py
+++ prime_no_1's_bin.py
@@ -13,13 +13,10 @@ count=0
 
 for i in range(l,r+1):
   string='{:2b}'.format(i)
-  print(string)
   c=0
   for j in string:
     if j=='1':
       c+=1
-  print(c)
   if is_prime(c):
-    print('prime')
     count+=1
 print(count-1)
