bdea95bdd6e06e17a11d9912ab9d729b8786bc3d
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-06 13:19:08 +0530

    Update col_row_0.py

--- col_row_0.py
+++ col_row_0.py
@@ -6,10 +6,10 @@ for i in range(len(n_l)):
   for j in range(len(n_l[i])):
     if n_l[i][j]==0:
       for i2 in range(n):
-        if n_l[i2][j]!=9:
+        if n_l[i2][j]!=0:
           n_l[i2][j]=9
       for j2 in range(m):
-        if n_l[i][j2]!=9:
+        if n_l[i][j2]!=0:
           n_l[i][j2]=9
 for i in range(len(n_l)):
   for j in range(len(n_l[i])):
