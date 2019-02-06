328f69ad09077294c9592e8a9ea6672505d2679d
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-06 17:48:24 +0530

    Update hunter_23.py

--- hunter_23.py
+++ hunter_23.py
@@ -20,3 +20,13 @@ while [n-1,m-1] not in pos:
       t.append([x[0],x[1]+1])
     elif x[0]+1<n:
       temp_out.append(a[x[0]+1][x[1]]+out[i])
+      t.append([x[0]+1,x[1]])
+    elif x[1]+1<m:
+      temp_out.append(a[x[0]][x[1]+1]+out[i])
+      t.append([x[0],x[1]+1])
+    i+=1
+  pos=t
+  t=[]
+  out=temp_out
+  temp_out=[]
+print(max(out))
