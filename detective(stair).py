5dcbaa4a8779de15905abd7ed40f6d97e7825307
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-07 14:01:19 +0530

    Update detective(stair).py

--- detective(stair).py
+++ detective(stair).py
@@ -3,7 +3,8 @@ a=list(map(eval,input().split()))
 c=[]
 out=0
 for i in range(1,n):
-  if a[i-1]<a[i]:
-    out+=a[i-1]
-    c.append(out)
-print(sum(c))
+  t=a[:i]
+  for j in t:
+    if j<a[i]:
+      out+=j
+print(out)
