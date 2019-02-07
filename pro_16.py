b812dbcfcd5a3ecddab021aac830298a4a44245b
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-07 16:56:11 +0530

    Create pro_16.py

---
+++ pro_16.py
@@ -0,0 +1,10 @@
+n=int(input())
+nos=list(map(eval,input().split()))
+cdl=[1]*n
+for i in range(n-1):
+  for i in range(0,n-i-1):
+    if nos[i]<nos[i+1]:
+      cdl[i+1]=cdl[i]+1
+    elif nos[i+1]<nos[i]:
+      cdl[i]=cdl[i+1]+1
+print(sum(cdl))
