299fd0042593467ad068b3c6ea8b244c9f9072fd
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-07 14:32:28 +0530

    Create pro_12.py

---
+++ pro_12.py
@@ -0,0 +1,12 @@
+n,m=map(eval,input().split())
+A=list(map(eval,input().split()))
+A.insert(0,0)
+p=[]
+for i in range(m):
+  p.append(list(map(eval,input().split())))
+
+for i in p:
+  out=0
+  for j in range(i[0],i[1]+1):
+    out+=A[j]
+  print(out)
