014f5a70b1fb1b438906173346431c907f4ef814
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-09 22:26:38 +0530

    Update pro_21.py

--- pro_21.py
+++ pro_21.py
@@ -7,10 +7,8 @@ def avg(temp):
 n=int(input())
 a=list(map(int,input().split()))
 for i in range(1,n):
-    print(a[:i],a[i:])
     s1=avg(a[:i])
     s2=avg(a[i:])
-    print(s1,s2)
     if s1==s2:
         print('yes')
         break
