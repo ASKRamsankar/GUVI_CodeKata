4948d00101456193e9f02811ee64acbd486ecf16
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-11 17:36:32 +0530

    Create pro_30.py

---
+++ pro_30.py
@@ -0,0 +1,17 @@
+n=input()
+f=0
+for i in range(1,len(n)):
+  j=0
+  while j<len(n) and len(n[:j]+n[j+i:])==len(n)-i:
+    no=n[:j]+n[j+i:]
+    print(no)
+    no=int(no)
+    if no>-1 and no%8==0:
+      print('yes')
+      f=1
+      break
+    j+=1
+  if f==1:
+    break
+else:
+  print('no')
