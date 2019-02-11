a5eb920223bbd668e24e89a7b8fd7d9c208ed787
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-11 17:07:46 +0530

    Update pro_29.py

--- pro_29.py
+++ pro_29.py
@@ -1,15 +1,16 @@
 def digit_s(t):
   o=0
-  for i in str(t):
+  t=str(t)
+  for i in t:
     o+=int(i)
   return o
 no=int(input())
 c=0
 out=[]
-for i in range(0,101):
-  if abs(no-i)+digit_s(no-i)==no:
+for i in range(1,101):
+  if no-i>0 and (no-i)+digit_s(no-i)==no:
       c+=1
-      out.append(no-i)
+      out.append(abs(no-i))
 print(c)
 for i in out:
   print(i)
