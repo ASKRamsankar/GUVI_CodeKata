2d5e8abbcc44d09bd30245c4a3ccadcb2211b618
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-11 14:14:00 +0530

    Create longest_increasing_subsequence.py

---
+++ longest_increasing_subsequence.py
@@ -0,0 +1,19 @@
+n=int(input())
+nos=list(map(eval,input().split()))
+j=0
+cont=1
+m=0
+i=0
+while i<n:
+  c=1
+  small=nos[i]
+  j=i+1
+  while j<n and len(nos[i:])>m:
+    if small<nos[j]:
+      small=nos[j]
+      c+=1
+    j+=1
+  if c>m:
+    m=c
+  i+=1
+print(m)
