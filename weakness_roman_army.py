5cec8ea034a927becfefd2b15a3c5161fe60b3a1
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-13 23:00:02 +0530

    Update weakness_roman_army.py

--- weakness_roman_army.py
+++ weakness_roman_army.py
@@ -1,9 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
 c=0
-for i in range(n-2):
-  for j in range(i+1,n-1):
-    for k in range(j+1,n):
+for i in range(len(a)-2):
+  for j in range(i+1,len(a)-1):
+    for k in range(j+1,len(a)):
       if a[i]>a[j]and a[j]>a[k]:
         c+=1
 print(c)
