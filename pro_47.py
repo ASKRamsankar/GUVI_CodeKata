c9db31a723b855b2a36c6525cf720db254a555c6
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-15 23:31:39 +0530

    Update pro_47.py

--- pro_47.py
+++ pro_47.py
@@ -1,5 +1,17 @@
 n,k=map(int,input().split())
-if 10%(n%10)==0:
+if n%10==0:
+  n=str(n)
+  c=0
+  for i in range(len(n)-1,-1,-1):
+    if n[i]=='0':
+      c+=1
+  if k<=c:
+    print(n)
+  else:
+    n=n[:-c]
+    n=n+'0'*k
+    print(n)
+elif 10%(n%10)==0:
   no=int('1'+'0'*k)
   while True:
     if no%n==0:
