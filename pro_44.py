699f8a923dc1ba66452774479b460583afb41281
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-15 15:55:25 +0530

    Update pro_44.py

--- pro_44.py
+++ pro_44.py
@@ -1,8 +1,8 @@
 n,p,q,r=map(int,input().split())
 a=list(map(int,input().split()))
 m=min(a)
-for i in range(0,len(a)-2):
-  for j in range(i,len(a)-1):
+for i in range(0,len(a)):
+  for j in range(i,len(a)):
     for k in range(j,len(a)):
       val=p*a[i]+q*a[j]+r*a[k]
       if val>m:
