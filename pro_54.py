5c898de60968da2b9465936c9367cd7b0ff9a9da
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-16 23:18:43 +0530

    Create pro_54.py

---
+++ pro_54.py
@@ -0,0 +1,22 @@
+n,k=map(int,input().split())
+need=list(map(int,input().split()))
+avail=list(map(int,input().split()))
+cookies=0
+f=0
+while f!=1:
+  c=0
+  for i in range(n):
+    if avail[i]>=need[i]:
+      avail[i]=avail[i]-need[i]
+      c+=1
+    else:
+      if k>=need[i]-avail[i]:
+        k-=need[i]-avail[i]
+        avail[i]=0
+        c+=1
+      else:
+        f=1
+        break
+  if c==n:
+    cookies+=1
+print(cookies)
