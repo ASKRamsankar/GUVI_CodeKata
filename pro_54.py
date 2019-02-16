14501e8662292fd9eb4cf839f5f81baf9011f0d5
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-16 23:23:17 +0530

    Update pro_54.py

--- pro_54.py
+++ pro_54.py
@@ -1,8 +1,12 @@
+import sys
 n,k=map(int,input().split())
 need=list(map(int,input().split()))
 avail=list(map(int,input().split()))
 cookies=0
 f=0
+if n==1:
+  print((avail[0]+k)//need[0])
+  sys.exit(0)
 while f!=1:
   c=0
   for i in range(n):
