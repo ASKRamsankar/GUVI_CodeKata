eb8966ca6f9f50bfb8b0d3f4083be0fe66539eb9
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-18 22:57:47 +0530

    Update pro_59.py

--- pro_59.py
+++ pro_59.py
@@ -1,6 +1,13 @@
-scale=input()+input()
-l_scale,r_scale=scale.split('|')
-if len(l_scale)==len(r_scale):
-  print(scale)
+scale=input()
+wei=input()
+scale1=scale+wei
+scale2=wei+scale
+
+l_scale1,r_scale1=scale1.split('|')
+l_scale2,r_scale2=scale2.split("|")
+if len(l_scale1)==len(r_scale1):
+  print(scale1)
+elif len(l_scale2)==len(r_scale2):
+  print(scale2)
 else:
   print('impossible')
